b6a6a8fcd8ebe98c59e62c3fa0664f59619ae000
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-05-11 17:32:23 -0700

    profile table updated and pg module installed

--- src/db/migrations/03ProfileTable.js
+++ src/db/migrations/03ProfileTable.js
@@ -25,7 +25,7 @@ exports.up = async knex => {
 		table.string('zipCode').notNullable()
 		table.string('dob').notNullable()
 		table.integer('rating').defaultTo(5)
-		table.json("app_metadata").defaultTo(JSON.stringify("{roles:['new_user'], permissions:['create:own_content', 'edit:own_content', 'upload:own_media']}"));
+		table.json("app_metadata").defaultTo(JSON.stringify("{roles:['car_renter'], permissions:['create:own_content', 'edit:own_content', 'upload:own_media']}"));
 	})
 }
 

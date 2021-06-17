053378e53447388f49e9a914790f3c5ea04e11a0
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-06-17 16:54:17 -0700

    update user in resolver

--- src/db/user.js
+++ src/db/user.js
@@ -3,6 +3,7 @@ const db = require('../config/knex');
 module.exports = {
 	findOne,
 	create,
+	update
 }
 
 function findOne(filter) {
@@ -13,3 +14,9 @@ async function create(input) {
 	const [id] = await db('user').insert(input).returning('id')
 	return findOne({ id })
 }
+
+async function update(input, identity) {
+	const [id] = await db('user').where(identity).update(input).returning('id')
+	return findOne({ id })
+}
+
